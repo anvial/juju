28b551a4b577a0fd3f9f3da1b7bde4feb61be787
Joseph Phillips <joe.phillips@canonical.com>
2025-11-25 12:26:21 +0100

    feat: compat with 3.6 for machainemanager facade

--- api/facadeversions.go
+++ api/facadeversions.go
@@ -62,7 +62,10 @@ var facadeVersions = facades.FacadeVersions{
 	"LeadershipService":            {2},
 	"Logger":                       {1},
 	"MachineActions":               {1},
-	"MachineManager":               {11},
+	// Note that this version of Juju does not implement version 10
+	// of the facade, but 3.6 does. Care must be taken not to break
+	// client compatibility with the prior version.
+	"MachineManager":               {10, 11},
 	"Machiner":                     {5, 6},
 	"MigrationFlag":                {1},
 	"MigrationMaster":              {4, 5},
