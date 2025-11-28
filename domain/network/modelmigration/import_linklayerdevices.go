4d93ff055df721ba6b93235964df8e39c2679048
Nicolas Vinuesa <vinuesa.nico@gmail.com>
2025-11-28 12:41:02 +0100

    chore: refactor network modelmigration import operations
    During model import we have a problem, related with the order of the
    import operations where the import of machines would fail because of
    missing import of availability zones. This patch solves it by splitting
    the network import again into LLDs that run *after* the machine import
    and the subnets (plus spaces and AZs) that runs *before* machines.

--- domain/network/modelmigration/import_linklayerdevices.go
+++ domain/network/modelmigration/import_linklayerdevices.go
@@ -26,132 +26,44 @@ type Coordinator interface {
 	Add(modelmigration.Operation)
 }
 
-// RegisterImport registers the import operations with the given coordinator.
-func RegisterImport(coordinator Coordinator, logger logger.Logger) {
+// RegisterLinkLayerDevicesImport registers the import operations with the given coordinator.
+func RegisterLinkLayerDevicesImport(coordinator Coordinator, logger logger.Logger) {
 	coordinator.Add(&importOperation{
 		logger: logger,
 	})
 }
 
-// ImportService provides a subset of the network domain service
-// methods needed for spaces and subnets import.
-type ImportService interface {
-	AddSpace(ctx context.Context, space corenetwork.SpaceInfo) (corenetwork.SpaceUUID, error)
-	Space(ctx context.Context, uuid corenetwork.SpaceUUID) (*corenetwork.SpaceInfo, error)
-	AddSubnet(ctx context.Context, args corenetwork.SubnetInfo) (corenetwork.Id, error)
-}
-
-// MigrationService defines methods needed to import and export
+// LinkLayerDevicesMigrationService defines methods needed to import
 // link layer devices as part of model migration.
-type MigrationService interface {
+type LinkLayerDevicesMigrationService interface {
 	// ImportLinkLayerDevices imports the given link layer device data into
 	// the model.
 	ImportLinkLayerDevices(ctx context.Context, data []internal.ImportLinkLayerDevice) error
-
-	// ImportCloudServices imports cloud service metadata into the model using the provided context and service data.
-	ImportCloudServices(ctx context.Context, services []internal.ImportCloudService) error
 }
 
 type importOperation struct {
 	modelmigration.BaseOperation
 
-	importService    ImportService
-	migrationService MigrationService
+	migrationService LinkLayerDevicesMigrationService
 	logger           logger.Logger
 }
 
 // Name returns the name of this operation.
 func (i *importOperation) Name() string {
-	return "import networks"
+	return "import link layer devices"
 }
 
 // Setup implements Operation.
 func (i *importOperation) Setup(scope modelmigration.Scope) error {
 	st := state.NewState(scope.ModelDB(), i.logger)
-	i.importService = service.NewService(
-		st,
-		i.logger,
-	)
 	i.migrationService = service.NewMigrationService(st, i.logger)
 	return nil
 }
 
-// Execute the import of the spaces, subnets and link layer devices
-// contained in the model.
+// Execute the import of the link layer devices contained in the model.
 func (i *importOperation) Execute(ctx context.Context, model description.Model) error {
-	spaceIDsMap, err := i.importSpaces(ctx, model.Spaces())
-	if err != nil {
-		return errors.Capture(err)
-	}
-	if err := i.importSubnets(ctx, model.Subnets(), spaceIDsMap); err != nil {
-		return errors.Capture(err)
-	}
 	if err := i.importLinkLayerDevices(ctx, model.LinkLayerDevices(), model.IPAddresses()); err != nil {
 		return errors.Capture(err)
-	}
-	return nil
-}
-
-func (i *importOperation) importSpaces(ctx context.Context, modelSpaces []description.Space) (map[string]corenetwork.SpaceUUID, error) {
-	spaceIDsMap := make(map[string]corenetwork.SpaceUUID)
-	for _, space := range modelSpaces {
-		// The default space should not have been exported, but be defensive.
-		if space.Name() == corenetwork.AlphaSpaceName.String() {
-			continue
-		}
-		spaceInfo := corenetwork.SpaceInfo{
-			ID:         corenetwork.SpaceUUID(space.UUID()),
-			Name:       corenetwork.SpaceName(space.Name()),
-			ProviderId: corenetwork.Id(space.ProviderID()),
-		}
-		spaceID, err := i.importService.AddSpace(ctx, spaceInfo)
-		if err != nil {
-			return nil, errors.Errorf("creating space %s: %w", space.Name(), err)
-		}
-		// Update the space IDs mapping, which we need for subnets
-		// import. We do this for the pre-4.0 migrations, where
-		// spaces have their ID set but not their UUID. If their UUID
-		// is set then we use it to keep a consistent mapping.
-		if space.Id() != "" {
-			spaceIDsMap[space.Id()] = spaceID
-		} else {
-			spaceIDsMap[spaceID.String()] = spaceID
-		}
-	}
-	return spaceIDsMap, nil
-}
-
-func (i *importOperation) importSubnets(
-	ctx context.Context,
-	modelSubnets []description.Subnet,
-	spaceIDsMap map[string]corenetwork.SpaceUUID,
-) error {
-
-	for _, subnet := range modelSubnets {
-		subnetInfo := corenetwork.SubnetInfo{
-			ID:                corenetwork.Id(subnet.UUID()),
-			CIDR:              subnet.CIDR(),
-			ProviderId:        corenetwork.Id(subnet.ProviderId()),
-			VLANTag:           subnet.VLANTag(),
-			AvailabilityZones: subnet.AvailabilityZones(),
-			ProviderNetworkId: corenetwork.Id(subnet.ProviderNetworkId()),
-		}
-
-		importedSpaceID, ok := spaceIDsMap[subnet.SpaceID()]
-		if ok {
-			space, err := i.importService.Space(ctx, importedSpaceID)
-			if err != nil {
-				return errors.Errorf("retrieving space with ID %s to import subnet %s: %w", importedSpaceID, subnet.ID(), err)
-			}
-			subnetInfo.SpaceID = importedSpaceID
-			subnetInfo.SpaceName = space.Name
-			subnetInfo.ProviderSpaceId = space.ProviderId
-		}
-
-		_, err := i.importService.AddSubnet(ctx, subnetInfo)
-		if err != nil {
-			return errors.Errorf("creating subnet %s: %w", subnet.CIDR(), err)
-		}
 	}
 	return nil
 }
@@ -262,11 +174,6 @@ func (i *importOperation) ensureAddressWithCIDR(address description.IPAddress, a
 		return fmt.Sprintf("%s/128", address.Value())
 	}
 	return address.Value()
-}
-
-// ptr returns a reference to a copied value of type T.
-func ptr[T any](i T) *T {
-	return &i
 }
 
 func nilZeroPtr[T comparable](v T) *T {
