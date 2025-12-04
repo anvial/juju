2ddaaa1a5f124420c01dbe7b400c79377c5cc76d
Guillaume Fouillet <guillaume.fouillet@canonical.com>
2025-12-04 17:15:43 +0100

    refactor(secret): split `v_secret_owner` from `v_secret_metadata`
    This patch separates the owner information into a new dedicated view `v_secret_owner` in
     `domain/schema/model/sql/0037-secret-metadata.PATCH.sql`.

--- domain/schema/model/sql/0039-secret-metadata.PATCH.sql
+++ domain/schema/model/sql/0039-secret-metadata.PATCH.sql
@@ -1,17 +1,17 @@
+CREATE VIEW v_secret_owner AS
+SELECT 'model' AS owner_kind, (SELECT uuid FROM model LIMIT 1) AS owner_id, so.label, so.secret_id
+FROM   secret_model_owner AS so
+UNION ALL
+SELECT 'application' AS owner_kind, a.name AS owner_id, so.label, so.secret_id
+FROM   secret_application_owner AS so
+JOIN   application AS a ON so.application_uuid = a.uuid
+UNION ALL
+SELECT 'unit' AS owner_kind, u.name AS owner_id, so.label, so.secret_id
+FROM   secret_unit_owner AS so
+JOIN   unit AS u ON so.unit_uuid = u.uuid;
+
+
 CREATE VIEW v_secret_metadata AS
-WITH
-secret_owner AS (
-    SELECT 'model' AS owner_kind, (SELECT uuid FROM model LIMIT 1) AS owner_id, so.label, so.secret_id
-    FROM   secret_model_owner AS so
-    UNION ALL
-    SELECT 'application' AS owner_kind, a.name AS owner_id, so.label, so.secret_id
-    FROM   secret_application_owner AS so
-    JOIN   application AS a ON so.application_uuid = a.uuid
-    UNION ALL
-    SELECT 'unit' AS owner_kind, u.name AS owner_id, so.label, so.secret_id
-    FROM   secret_unit_owner AS so
-    JOIN   unit AS u ON so.unit_uuid = u.uuid
-)
 SELECT
     sm.secret_id,
     sm.version,
@@ -32,4 +32,4 @@ JOIN        secret_revision AS sr ON sm.secret_id = sr.secret_id
 LEFT JOIN   secret_revision_expire AS sre ON sre.revision_uuid = sr.uuid
 LEFT JOIN   secret_rotate_policy AS rp ON rp.id = sm.rotate_policy_id
 LEFT JOIN   secret_rotation AS sro ON sro.secret_id = sm.secret_id
-LEFT JOIN   secret_owner AS so ON so.secret_id = sm.secret_id;
+LEFT JOIN   v_secret_owner AS so ON so.secret_id = sm.secret_id;
--- domain/schema/model_schema_test.go
+++ domain/schema/model_schema_test.go
@@ -461,6 +461,7 @@ func (s *modelSchemaSuite) TestModelViews(c *tc.C) {
 		"v_agent_binary_store",
 		"v_all_unit_address",
 		"v_secret_metadata",
+		"v_secret_owner",
 	)
 	got := readEntityNames(c, s.DB(), "view")
 	c.Assert(got, tc.SameContents, expected.SortedValues(), tc.Commentf(
