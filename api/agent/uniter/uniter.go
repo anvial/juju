7318b55dd501ffb4a5e9622bd6cdbe2e4ad7caf2
Heather Lanigan <heather.lanigan@canonical.com>
2025-12-09 19:31:14 +0000

    refactor: delete unused method

--- api/agent/uniter/uniter.go
+++ api/agent/uniter/uniter.go
@@ -11,7 +11,6 @@ import (
 	"github.com/juju/errors"
 	"github.com/juju/names/v6"
 
-	"github.com/juju/juju/api"
 	"github.com/juju/juju/api/base"
 	"github.com/juju/juju/api/common"
 	"github.com/juju/juju/api/types"
@@ -65,17 +64,6 @@ func NewClient(
 		facade:             facadeCaller,
 		unitTag:            authTag,
 	}
-}
-
-// NewFromConnection returns a version of the Connection that provides
-// functionality required by the uniter worker if possible else a non-nil error.
-func NewFromConnection(c api.Connection) (*Client, error) {
-	authTag := c.AuthTag()
-	unitTag, ok := authTag.(names.UnitTag)
-	if !ok {
-		return nil, errors.Errorf("expected UnitTag, got %T %v", authTag, authTag)
-	}
-	return NewClient(c, unitTag), nil
 }
 
 // BestAPIVersion returns the API version that we were able to
